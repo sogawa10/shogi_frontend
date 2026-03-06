9cf121d363f247745fe4b7b95c065706a285672d
Soki-1010 <sogawa.doshisha@gmail.com>
2026-03-07 00:48:45 +0900

    新規登録ページの作成（API通信は未実装）

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 // Routes → 複数のRouteをまとめるためのコンテナ．
 // Route → pathに一致するURLが呼ばれると，指定したコンポーネントを表示．
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-// コンポーネントをインポート
+// コンポーネントをインポート．
 import AuthPage from './pages/AuthPage.tsx';
 import LoginPage from './pages/LoginPage.tsx';
 import RegisterPage from './pages/RegisterPage.tsx';
--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -1,21 +1,21 @@
 import './AuthPage.css'
-// ボタンをクリックしたときに画面遷移を行う関数
+// ボタンをクリックしたときに画面遷移を行う関数．．
 import { useNavigate } from 'react-router-dom';
 
 function AuthPage() {
-  // 関数をnavigateに代入（以降navigate()で使用できる）
+  // 関数をnavigateに代入（以降navigate()で使用できる）．
   const navigate = useNavigate();
   return (
     <>
-      <h1>みんなの将棋AI</h1>
-      <p>
+      <h1 className="hero-title">みんなの将棋AI</h1>
+      <p className="hero-description">
         みんなが作った将棋AIと対局したり、
         AI同士を対局させたりできるコミュニティサイトです。
       </p>
-      <button className="btn login" onClick={() => navigate("/login")}>
+      <button className="button login-button" onClick={() => navigate("/login")}>
         ログイン
       </button>
-      <button className="btn register" onClick={() => navigate("/register")}>
+      <button className="button register-button" onClick={() => navigate("/register")}>
         新規登録
       </button>
     </>
